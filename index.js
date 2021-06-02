4d5d25f42785e95c973408bfb19b1fa64fd17a78
Bryan Civils <hermanbciv@gmail.com>
2021-06-02 00:12:15 -0400

    Updates to Markdown output.   Minor updates elsewhere

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const questions = [
     {
       type: 'input',
       name: 'usage',
-      message: 'Enter instructions or exmaples on how the project can be used'
+      message: 'Enter instructions or examples on how the project can be utilized'
     },
     {
       type: 'input',
@@ -47,17 +47,17 @@ const questions = [
       message: 'Enter instructions and guidelines for testing project elements'
     },
     {
-      type: 'checkbox',
+      type: 'list',
       name: 'badge',
       message: 'Select all applicable licesnses for your project',
-      choices: ['CC-BY-SA-4.0', 'Apache Licesnse 2.0', 'GNU GPLv3', 'ISC', 'MIT License', 'None / Not Applicable']
+      choices: ['CC-BY-SA-4.0', 'Apache License 2.0', 'GNU GPLv3', 'ISC', 'MIT License', 'None / Not Applicable']
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(readMeContent) {
   fs.writeFile('./output/README.md', readMeContent, (err) =>
-    err ? console.error(err) : console.log('Congratulations!\nYour README file has been generated successfully!'));
+    err ? console.error(err) : console.log('Congratulations!\nYour README file has been generated successfully'));
 }
 
 // TODO: Create a function to initialize app
@@ -65,11 +65,7 @@ function init() {
     inquirer
       .prompt(questions)
       .then((data) => {
-        //const filename = 'README.md';
         const readMeContent = generateMarkdown(data).trim();
-        //console.log(`OUTPUT:  ${readMeContent}`);
-        //console.log(`OUTPUT:  ${generateMarkdown(data)}`);
-        //console.log(`OUTPUT:  ${generateMarkdown()}`)
         writeToFile(readMeContent)
       });
 }
