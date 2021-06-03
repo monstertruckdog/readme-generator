9d4a381a5554cff454b20b61ca478e3076716425
Bryan Civils <hermanbciv@gmail.com>
2021-06-03 02:01:23 -0400

    Minor changes to index, generateMarkdown and README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-perhaps this workedperhaps this workedperhaps this workedperhaps this workedperhaps this workedperhaps this worked
+# Professional README Generator
+
+### Description
+
+To help reduce the time and effort it takes for a develper to organize and properly format information about their application's README file, we have created a README generator.  This application, written in node.js, will prompt the developer to enter the necessary information about their application or project and create a professional README document for them.  
+
+### License
+*under review*
+
+### Installation
+
+Installation consists of simply having a couple of Javascript files, downloading a package from [npm](https://www.npmjs.com/) and running the `node` command on  one of those JS files:
+
+1. [Clone or fork the repository](https://github.com/monstertruckdog/readme-generator) to a local directory
+2. In your terminal, navigate to the directory and execute the following command:
+
+ ```
+ npm i inquirer
+ ```
+3. Once installation is completed, execute another command in the terminal:
+
+ ```
+  node index.js
+```
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (data.badge) {
+function renderLicenseBadge(datas) {
+  /*
+  if (datas.data.badge) {
     renderLicenseLink();
-    return `![license](${badgeUrl})`
+    const badgeConstruct = `![license](${badgeUrl})`
+    console.log(`renderLicenseBadge:  ${badgeConstruct}`);
   }
+  */
+  renderLicenseLink()
+  const badgeConstruct = `![license](${badgeUrl})`
+  console.log(`renderLicenseBadge:  ${badgeConstruct}`);
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseLink(data) {
+  /*
   if (data.badge) {
-    const badgeURL = `https://img.shields.io/badge/license-${data.badge}-informational`
-    return badgeUrl
+    const badgeUrl = `https://img.shields.io/badge/license-${data.badge}-informational`
+    console.log(`renderLicenseLink:  ${badgeUrl}`);
   }
+  */
+  const badgeUrl = `https://img.shields.io/badge/license-${data.badge}-informational`
+  console.log(`renderLicenseLink:  ${badgeUrl}`);
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
+function renderLicenseSection() {
   if (data.badge) {
-    const badgeFinalAssembly = renderLicenseBadge()
-    const badgeComplete = `### License
-                          ${badgeFinalAssembly}\n\n`
-    return badgeComplete
+    renderLicenseBadge()
+    console.log(`renderLicenseSection:  ${badgeConstruct}`)
+    //const badgeComplete = `### License
+    //                      ${badgeConstruct}\n\n`
+    return `### License
+    ${badgeConstruct}\n\n`
   }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  renderLicenseSection();
+  //renderLicenseSection();
+  const badgeConstruct = renderLicenseBadge()
   return `
 # ${data.projectname}\n
-${badgeComplete}
 ### Description\n
 ${data.description}\n\n
+### License
+${badgeConstruct}\n\n
 ### Index\n
 - [Installation](#installation)
 - [Usage Information](#usage)
